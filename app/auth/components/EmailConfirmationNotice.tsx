45e849c99180aba468cc8120a5d4943d19e0d871
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-11-25 23:20:41 +0800

    Align logo

--- app/auth/components/EmailConfirmationNotice.tsx
+++ app/auth/components/EmailConfirmationNotice.tsx
@@ -1,6 +1,6 @@
 import { Button } from "app/components/Button"
 import { Card } from "app/components/Card"
-import { Logo } from "app/components/Logo"
+import { SiRoots } from "react-icons/si"
 import { StyledLink } from "app/components/StyledLink"
 import { Link } from "blitz"
 import React from "react"
@@ -14,7 +14,7 @@ export const EmailConfirmationNotice = () => (
       <div className="text-center">
         <Link href="/" passHref>
           <a className="inline-block">
-            <Logo className="w-auto h-12" />
+            <SiRoots color="yellow" size="40" />
           </a>
         </Link>
       </div>
@@ -29,8 +29,8 @@ export const EmailConfirmationNotice = () => (
     <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
       <Card>
         <p className="text-center">
-          You've successfully signed up. <br />
-          We've sent you a confirmation email.
+          You&apos;ve successfully signed up. <br />
+          We&apos;ve sent you a confirmation email.
         </p>
         <br />
         <Link href="login">
