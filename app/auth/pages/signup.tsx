bf30bd5a273a82a99a9d05c9fd7a2721de90e290
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-11-25 23:22:12 +0800

    Update signup page to current API

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react"
-import { BlitzPage, GetServerSideProps } from "blitz"
-import Layout from "app/layouts/Layout"
+import { useState } from "react"
+import { useRouter, BlitzPage, Routes } from "blitz"
+import Layout from "app/core/layouts/Layout"
 import { SignupForm } from "app/auth/components/SignupForm"
-import { EmailConfirmationNotice } from "../components/EmailConfirmationNotice"
-import { ensureUnauthenticated } from "app/guards/ensureUnauthenticated"
-
-export const getServerSideProps: GetServerSideProps = ensureUnauthenticated
+import { EmailConfirmationNotice } from "app/auth/components/EmailConfirmationNotice"
 
 const SignupPage: BlitzPage = () => {
   const [signedUp, setSignedUp] = useState(false)
@@ -17,6 +14,7 @@ const SignupPage: BlitzPage = () => {
   )
 }
 
+SignupPage.redirectAuthenticatedTo = "/"
 SignupPage.getLayout = (page) => <Layout title="Sign Up">{page}</Layout>
 
 export default SignupPage
