c37e9588bdbe87b3afb195a821243cfa35ddd3e5
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-11-25 23:23:18 +0800

    Update auth utils to current API

--- app/auth/auth-utils.ts
+++ app/auth/auth-utils.ts
@@ -1,26 +1,8 @@
 import { AuthenticationError, SecurePassword } from "blitz"
-import SecurePassword from "secure-password"
 import db, { User } from "db"
 import { EmailUsedError } from "app/errors/emailUsed"
 import { UnconfirmedEmailError } from "app/errors/unconfirmedEmail"
-
-const SP = new SecurePassword()
-
-export const hashPassword = async (password: string) => {
-  const hashedBuffer = await SP.hash(Buffer.from(password))
-
-  return hashedBuffer.toString("base64")
-}
-
-export const verifyPassword = async (hashedPassword: string, password: string) => {
-  try {
-    return await SP.verify(Buffer.from(password), Buffer.from(hashedPassword, "base64"))
-  } catch (error) {
-    console.error(error)
-
-    return false
-  }
-}
+import { Login } from "./validations"
 
 export const ensureUserConfirmed = (user: Pick<User, "confirmedAt">) => {
   if (!user.confirmedAt) {
@@ -29,7 +11,7 @@ export const ensureUserConfirmed = (user: Pick<User, "confirmedAt">) => {
 }
 
 export const ensureUserEmailNotUsed = async (email: string) => {
-  const user = await db.user.findOne({ where: { email } })
+  const user = await db.user.findFirst({ where: { email } })
 
   if (!!user) {
     throw new EmailUsedError({ email })
