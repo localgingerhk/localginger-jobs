97888b6f8ea08e191b56d8508114295176f112bf
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-11-25 23:21:43 +0800

    Update login page to current API

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect } from "react"
-import { useRouter, BlitzPage, GetServerSideProps } from "blitz"
-import Layout from "app/layouts/Layout"
+import { useRouter, BlitzPage } from "blitz"
+import Layout from "app/core/layouts/Layout"
 import { LoginForm } from "app/auth/components/LoginForm"
-import { ensureUnauthenticated } from "app/guards/ensureUnauthenticated"
-
-export const getServerSideProps: GetServerSideProps = ensureUnauthenticated
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
 
   return (
     <div>
-      <LoginForm onSuccess={() => router.push("/")} />
+      <LoginForm
+        onSuccess={(_user) => {
+          const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
+          router.push(next)
+        }}
+      />
     </div>
   )
 }
 
+LoginPage.redirectAuthenticatedTo = "/"
 LoginPage.getLayout = (page) => <Layout title="Log In">{page}</Layout>
 
 export default LoginPage
