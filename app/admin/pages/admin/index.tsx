4d8d46c33b3afd25e50abedec681424adb9aa21a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-11-28 03:30:28 +0800

    Remove gssp use and wrap in Suspense

--- app/admin/pages/admin/index.tsx
+++ app/admin/pages/admin/index.tsx
@@ -1,36 +1,43 @@
 import React, { Suspense } from "react"
-import { BlitzPage, GetServerSideProps } from "blitz"
+import { BlitzPage, useRouter, Routes } from "blitz"
 import { AppLayout } from "app/layouts/AppLayout"
 import { JobsList } from "app/jobs/components/JobsList"
 import { Loading } from "app/components/Loading"
-import { ensureHasRole } from "app/guards/ensureHasRole"
 import { Role } from "app/users/role"
 import getUnpublishedJobs from "app/admin/queries/getUnpublishedJobs"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
-export const getServerSideProps: GetServerSideProps = async (context) =>
-  await ensureHasRole({ ...context, role: Role.ADMIN })
+const AdminComponent = () => {
+  const user = useCurrentUser()
+  const router = useRouter()
 
-const Admin: BlitzPage = () => {
+  if (user && user.role !== Role.ADMIN) {
+    router.push(Routes.LoginPage())
+  }
   return (
-    <React.Fragment>
+    <>
       <div>
         <h3 className="text-lg font-medium leading-6 text-gray-900">Manage All Jobs</h3>
         <p className="mt-1 text-sm leading-5 text-gray-500">
           Here you can publish all the submitted jobs.
         </p>
       </div>
-      <Suspense
-        fallback={
-          <div className="flex items-end justify-center h-12">
-            <Loading className="w-5 h-5 text-red-500" />
-          </div>
-        }
-      >
-        <JobsList query={getUnpublishedJobs} withAdminActions />
-      </Suspense>
-    </React.Fragment>
+      <JobsList query={getUnpublishedJobs} withAdminActions />
+    </>
   )
 }
+
+const Admin: BlitzPage = () => (
+  <Suspense
+    fallback={
+      <div className="flex items-end justify-center h-12">
+        <Loading className="w-5 h-5 text-red-500" />
+      </div>
+    }
+  >
+    <AdminComponent />
+  </Suspense>
+)
 
 Admin.getLayout = (page) => <AppLayout title="Dashboard">{page}</AppLayout>
 
