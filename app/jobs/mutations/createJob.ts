a6501fff3e9ca5d808eaabbee514573111b030eb
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-12-07 18:05:23 +0800

    Remove unused imports

--- app/jobs/mutations/createJob.ts
+++ app/jobs/mutations/createJob.ts
@@ -1,7 +1,5 @@
 import { resolver } from "blitz"
 import db from "db"
-import { getActiveTags } from "../tags"
-import { SubmitJobInput, SubmitJobInputType } from "../validations"
 
 export default resolver.pipe(resolver.authorize(), async ({ tags, ...parsedInput }, ctx) => {
   const job = await db.job.create({
